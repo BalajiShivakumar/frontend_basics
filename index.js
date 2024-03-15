f810abefe28ba05e10f4d8e941191ac19aad706e
Balaji Sivakumar <Balaji@Balajis-MacBook-Pro.local>
2024-03-15 11:50:24 +0000

    second commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var a = 10;
+var b = 12;
+console.log(a+b);
+
+function add(a,b){
+  return a+b;
+}
+
+var ans = add(2,5);
+console.log(ans);
+var a = add(12,45);
+console.log(a);
+var b = add(34,56);
+console.log(b);
---
+++ script_1.js
@@ -0,0 +1,8 @@
+var x = 7;
+function getName(){
+  console.log("Hello from Console");
+
+}
+
+console.log(x);
+getName();
